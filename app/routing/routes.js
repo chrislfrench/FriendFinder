e679d47648f49bcc49f0d98d51f26d46035968bc
Chris French <clfrench84@gmail.com>
2017-04-28 23:03:56 -0700

    modal now workss

--- app/routing/routes.js
+++ app/routing/routes.js
@@ -1,23 +1,56 @@
+var friendData = require('../data/friends.js');
+
 var path = require("path");
 
-var friendsList = [];
-// var reservations = [];
+module.exports = function (app) {
 
-module.exports = function setupRoutes(app, express) {
-  console.log(__dirname);
-  app.use(express.static('../public'));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/home.html"));
+  app.get('/api/friends', function(req, res){
+    res.json(friendData);
   })
 
-  app.get("/api/friends", function(req, res) {
-    return res.json(friendsList);
-  });
 
-  app.post("/api/new", (req,res) => {
-    var newFriends = req.body;
-    friendsList.push(newFriends);
-    return res.json(friendsList);
-  });
-};
+  app.post('/api/friends', function(req, res){
+    var newFriend = req.body;
+
+    for(var i = 0; i < newFriend.scores.length; i++) {
+      if(newFriend.scores[i] == "1 (Strongly Disagree)") {
+        newFriend.scores[i] = 1;
+      } else if(newFriend.scores[i] == "5 (Strongly Agree)") {
+        newFriend.scores[i] = 5;
+      } else {
+        newFriend.scores[i] = parseInt(newFriend.scores[i]);
+      }
+    }
+
+    var differencesArray = [];
+
+    for(var i = 0; i < friendData.length; i++) {
+
+      var comparedFriend = friendData[i];
+      var totalDifference = 0;
+      
+      for(var k = 0; k < comparedFriend.scores.length; k++) {
+        var differenceOneScore = Math.abs(comparedFriend.scores[k] - newFriend.scores[k]);
+        totalDifference += differenceOneScore;
+      }
+
+      differencesArray[i] = totalDifference;
+    }
+
+    var bestFriendNum = differencesArray[0];
+    var bestFriendIndex = 0;
+
+    for(var i = 1; i < differencesArray.length; i++) {
+      if(differencesArray[i] < bestFriendNum) {
+        bestFriendNum = differencesArray[i];
+        bestFriendIndex = i;
+      }
+    }
+
+    friendData.push(newFriend);
+
+    res.json(friendData[bestFriendIndex]);
+  })
+}
+
+
